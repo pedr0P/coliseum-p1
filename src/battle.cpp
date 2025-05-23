36ad4da00f9a7ff1268064b04317b9bc797344ef
Pedro V. B. Pereira <pedrovbpereira20@gmail.com>
2025-05-23 12:37:39 -0300

    Start definitions of class methods

--- src/battle.cpp
+++ src/battle.cpp
@@ -1,1 +1,17 @@
 #include "battle.h"
+
+void Warrior::reset_debuff() {
+    this->atk_debuff = 0;
+}
+
+void Warrior::afflict(){}
+
+void Arena::combat(){}
+
+int Dice::RNG(int min, int max){
+    return 0;
+}
+
+int Dice::WRNG(int min, int max, int bias){
+    return 0;
+}
