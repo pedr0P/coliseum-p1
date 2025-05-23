55d0b90137dfc864dbf3bc82301de593d6ba726c
Pedro V. B. Pereira <pedrovbpereira20@gmail.com>
2025-05-23 12:27:49 -0300

    Create new function signatures according to task.md

--- src/battle.h
+++ src/battle.h
@@ -1,19 +1,55 @@
+#include <array>
+#include <string>
+
 //== Enumerations
-enum Action {
+enum action {
     ATTACK,
     DEFEND,
     HEAL
 };
 
-enum Stat {
+enum stat_field {
     STRENGTH,
     DEFENSE,
     HEALING
 };
 
-enum Player {
+enum player {
     ONE,
     TWO
 };
 
+/// Integer type for tracking stats.
+typedef unsigned stat;
+
+class Warrior {
+public:
+    std::string name;
+
+    stat health;
+    stat atk_debuff;
+
+    /// Static stats - Strength, Defense and Healing
+    std::array<stat, 3> stat_stats;
+
+    /// Dynamic stats - Attack and Health
+    std::array<stat, 2> dyn_stats;
+
+    void afflict();
+    void reset_debuff();
+};
+
+
+class Arena {
+    std::array<Warrior, 2> list_of_warriors;
+    
+    void combat();
+};
+
+class Dice{
+    /// Random Number Generator
+    int  RNG(int min, int max);
+    /// Weighted Random Number Generator
+    int WRNG(int min, int max, int bias);
+};
 
