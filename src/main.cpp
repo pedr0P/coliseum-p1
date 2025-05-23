b64bb3ea1f9a3b5ec9c09aeb2d444572cb34f890
Pedro V. B. Pereira <pedrovbpereira20@gmail.com>
2025-05-23 17:12:11 -0300

    Prototype of what main will be like.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "battle.cpp"
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello, github!";
+
+    Warrior a;
+    a.name = "A";
+    a.stat_stats = { 100, 100, 25};
+
+    Warrior b;
+    b.name = "B";
+    b.stat_stats = { 25, 50, 100 };
+
+    Arena coliseum;
+    coliseum.list_of_warriors = { a, b };
+    coliseum.combat();
 }
